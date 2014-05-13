c50c74c9c55cf5be567a21ae13d30464108635dd
Matt Chadburn <matt.chadburn@ft.com>
2014-05-13 12:52:11 +0100

    Add outputfields, a timeout and configurable host

--- src/clamo.js
+++ src/clamo.js
@@ -6,16 +6,35 @@ var request = require('superagent');
 // 
 var Clamo = function () {
     
-    var host = '/', 
-        offset = 0
-        limit = 10;
-    
-    // 
+    var opts = opts || {},
+        offset = 0,
+        limit = 10,
+        outputfields = {
+            id: true,
+            title: true,
+            content: 'html',
+            datepublished: true,
+            tags: 'visibleonly',
+            authorpseudonym: 'true'
+        }
+   
+    this.setHost = function (h) {
+        host = h;
+        return this;
+    }
+
+    /**
+     * Returns a promise of a HTTP request to the Clamo API
+     */
     var promiseOfClamo = function (url, params) {
         var deferred = Q.defer();
+
+        console.log(url, JSON.stringify([params]));
+
         request
             .post(url)
             .type('form')
+            .timeout(2000) // 2 seconds or die - config?
             .send({
                 request: JSON.stringify([params])
             })
@@ -30,22 +49,32 @@ var Clamo = function () {
 
     /** API */
 
+    /**
+     * Retrieves a sequence of posts from Clamo  
+     */
     this.search = function (query, p) { // TODO p is optional
         var params = { 
             action: 'search',
             arguments: { 
                 'query': query, 
                 'limit': p.limit || limit, 
-                'offset': p.offset || offset 
+                'offset': p.offset || offset,
+                'outputfields': outputfields
             }
         }
         return promiseOfClamo(host, params);
     } 
-    
+   
+    /**
+     * Retrieves a single post from a Clamo post id
+     */
     this.getPost = function (postId) {
         var params = {
-            action: 'getPost',
-            arguments: { 'id': postId }
+            'action': 'getPost',
+            'arguments': {
+                'id': postId,
+                'outputfields': outputfields
+            }
         }
         return promiseOfClamo(host, params);
     } 
@@ -56,5 +85,6 @@ var clamo = new Clamo();
 
 module.exports = {
     search: clamo.search, 
-    getPost: clamo.getPost
+    getPost: clamo.getPost,
+    withHost: clamo.setHost 
 }
