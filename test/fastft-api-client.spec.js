c6a7d53015c37855dab194bf4120d0406fe1aa27
Rhys Evans <rhys.evans@ft.com>
2014-05-21 14:47:50 +0100

    Increased test coverage of search

--- test/fastft-api-client.spec.js
+++ test/fastft-api-client.spec.js
@@ -33,26 +33,6 @@ describe('Clamo', function() {
         beforeEach(function () {
             Clamo.setHost('http://clamo.com/api');
         });
-
-        describe("client", function () {
-            it('should query Clamo API', function(done) {
-                jasmine.Ajax.stubRequest('http://clamo.com/api');
-                Clamo
-                    .getPost(12345)
-                    .then(function () {
-                        var request = jasmine.Ajax.requests.mostRecent();
-                        
-                        expect(request.url).toBe('http://clamo.com/api');
-                        expect(request.method).toBe('POST');
-
-                        var postBody = JSON.parse(request.data().request[0])[0]; 
-                        expect(postBody.action).toBe('getPost');
-                        expect(postBody.arguments.id).toBe(12345);
-                        done();
-                    }
-                );
-            });
-        });
 	
         xit('should catch errors returned by the service', function() {
                 // jasmine.Ajax.stubRequest('http://clamo.com/api').andReturn({
@@ -68,42 +48,123 @@ describe('Clamo', function() {
                 //     }
                 // );
         });
-        
-        describe(".search", function () {
-            it('should return a list of search results', function(done) {
-                jasmine.Ajax.stubRequest('http://clamo.com/api').andReturn({
-                    status: 200,
-                    responseText: fixtures.firstPage,
-                    ok: true
-                });
-                Clamo.search()
-                    .then(function (res) {
-                        expect(JSON.parse(res.response.text)[0].data.results[0].id).toBe(156442);
-                        expect(res.posts.length).toBe(10);
-                        expect(res.posts[0].constructor).toBe(Post);
-                        done();
-                    });
-            });
-            
-        });
-        
+
         describe(".getPost", function () {
-            
-            it('retrieve a given post', function(done) {
+            beforeEach(function () {
                 jasmine.Ajax.stubRequest('http://clamo.com/api').andReturn({
                     status: 200,
                     responseText: fixtures.getPost,
                     ok: true
                 });
+            })
+            it('should query Clamo API', function(done) {
+                Clamo
+                    .getPost(12345)
+                    .then(function () {
+                        var request = jasmine.Ajax.requests.mostRecent();
+                        
+                        expect(request.url).toBe('http://clamo.com/api');
+                        expect(request.method).toBe('POST');
+
+                        var postBody = JSON.parse(request.data().request[0])[0]; 
+                        expect(postBody.action).toBe('getPost');
+                        expect(postBody.arguments.id).toBe(12345);
+                        done();
+                    }
+                );
+            });
+            it('retrieve a given post', function(done) {
+
 
                 Clamo.getPost(147292)
                     .then(function (res) {
-                        expect(JSON.parse(res.response.text)[0].data.id).toBe(147292);
+                        expect(JSON.parse(res.response.text)[0].status).toBe('ok');
                         expect(res.post.constructor).toBe(Post);
+                        expect(res.post.id).toBe(147292);
+                        done();
+                    });
+            });
+            
+        });  
+              
+        describe(".search", function () {
+            beforeEach(function () {
+                jasmine.Ajax.stubRequest('http://clamo.com/api').andReturn({
+                    status: 200,
+                    responseText: fixtures.firstPage,
+                    ok: true
+                });
+            });
+
+            it('should query Clamo API', function(done) {
+                Clamo
+                    .search()
+                    .then(function () {
+                        var request = jasmine.Ajax.requests.mostRecent();
+                        
+                        expect(request.url).toBe('http://clamo.com/api');
+                        expect(request.method).toBe('POST');
+
+                        var postBody = JSON.parse(request.data().request[0])[0];
+                        expect(postBody.action).toBe('search');
+                        expect(postBody.arguments.outputfields).toEqual({
+                            id: true,
+                            title: true,
+                            content: 'html',
+                            abstract: 'html',
+                            datepublished: true,
+                            shorturl: true,
+                            metadata: true,
+                            tags: 'visibleonly',
+                            authorpseudonym: 'true'
+                        });
+                        done();
+                    }
+                );
+            });
+
+            it('should use default values for a request', function(done) {
+
+                Clamo.search()
+                    .then(function (res) {
+                        var request = jasmine.Ajax.requests.mostRecent();
+                        var args = JSON.parse(request.data().request[0])[0].arguments;
+                        expect(args.limit).toBe(10);
+                        expect(args.offset).toBe(0);
+                        expect(args.query).toBe('');
+                        done();
+                    });
+            });
+
+            it('should allow default values to be overridden', function(done) {
+                Clamo.search('testquery:forstuff', {
+                    limit: 30,
+                    offset: 60
+                })
+                    .then(function (res) {
+                        var request = jasmine.Ajax.requests.mostRecent();
+                        var args = JSON.parse(request.data().request[0])[0].arguments;
+                        expect(args.limit).toBe(30);
+                        expect(args.offset).toBe(60);
+                        expect(args.query).toBe('testquery:forstuff');
+                        done();
+                    });
+            });
+
+            it('should return a list of posts and teh original response', function(done) {
+                Clamo.search()
+                    .then(function (res) {
+                        expect(JSON.parse(res.response.text)[0].status).toBe('ok');
+
+                        expect(res.posts.length).toBe(10);
+                        expect(res.posts[0].constructor).toBe(Post);
+                        expect(res.posts[0].id).toBe(156442);
                         done();
                     });
             });
             
         });
+        
+
 	});
 });
