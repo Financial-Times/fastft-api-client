80629fdacfb78cb45bc37884552b345fcb57b9b9
Rhys Evans <rhys.evans@ft.com>
2014-05-28 17:37:25 +0100

    fixed tests

--- refreshtestdata.sh
+++ refreshtestdata.sh
@@ -5,8 +5,6 @@ fi
 
 OUTPUT_FIELDS=$(tr -d " \t\n\r" < outputfields.json)
 
-echo 'request=[{"action":"search","arguments":{"sort":"date","outputfields":'$OUTPUT_FIELDS',"query":"status:live","offset":0,"limit":10,"suppressdrafts":false}}]'
-
 curl -sG 'http://clamo.ftdata.co.uk/api'  -o $STUBS_LOCATION'search.json' --data-urlencode 'request=[{"action":"search","arguments":{"sort":"date","outputfields":'$OUTPUT_FIELDS',"query":"status:live","offset":0,"limit":10,"suppressdrafts":false}}]'
 curl -sG 'http://clamo.ftdata.co.uk/api'  -o $STUBS_LOCATION'nokia.json' --data-urlencode 'request=[{"action":"search","arguments":{"sort":"date","outputfields":'$OUTPUT_FIELDS',"query":"company:Nokia","offset":0,"limit":10,"suppressdrafts":false}}]'
 curl -sG 'http://clamo.ftdata.co.uk/api'  -o $STUBS_LOCATION'post.json' --data-urlencode 'request=[{"action":"getPost","arguments":{"id": 159222,"outputfields":'$OUTPUT_FIELDS'}}]'
