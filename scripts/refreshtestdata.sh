e619c54043f633c019dfdeb0de86f9747464023c
Rhys Evans <rhys.evans@ft.com>
2014-06-11 12:47:10 +0100

    renamed search test data to index

--- scripts/refreshtestdata.sh
+++ scripts/refreshtestdata.sh
@@ -1,5 +1,6 @@
 OUTPUT_FIELDS=$(tr -d " \t\n\r" < src/outputfields.json)
 
+curl -sG 'http://clamo.ftdata.co.uk/api'  -o 'test/stubs/index.json' --data-urlencode 'request=[{"action":"search","arguments":{"sort":"date","outputfields":'$OUTPUT_FIELDS',"query":"","offset":0,"limit":10,"suppressdrafts":false}}]'
 curl -sG 'http://clamo.ftdata.co.uk/api'  -o 'test/stubs/cameron.json' --data-urlencode 'request=[{"action":"search","arguments":{"sort":"date","outputfields":'$OUTPUT_FIELDS',"query":"cameron","offset":0,"limit":10,"suppressdrafts":false}}]'
 curl -sG 'http://clamo.ftdata.co.uk/api'  -o 'test/stubs/nokia.json' --data-urlencode 'request=[{"action":"search","arguments":{"sort":"date","outputfields":'$OUTPUT_FIELDS',"query":"company:Nokia","offset":0,"limit":10,"suppressdrafts":false}}]'
 curl -sG 'http://clamo.ftdata.co.uk/api'  -o 'test/stubs/post.json' --data-urlencode 'request=[{"action":"getPost","arguments":{"id": 159222,"outputfields":'$OUTPUT_FIELDS'}}]'
