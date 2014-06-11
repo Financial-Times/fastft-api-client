acd07633f1e51a4cbc6b0a7fdf2363b5732deaef
Rhys Evans <rhys.evans@ft.com>
2014-06-11 12:21:03 +0100

    testdata now provides examples of tag and search term searches

--- scripts/refreshtestdata.sh
+++ scripts/refreshtestdata.sh
@@ -1,6 +1,6 @@
 OUTPUT_FIELDS=$(tr -d " \t\n\r" < src/outputfields.json)
 
-curl -sG 'http://clamo.ftdata.co.uk/api'  -o 'test/stubs/search.json' --data-urlencode 'request=[{"action":"search","arguments":{"sort":"date","outputfields":'$OUTPUT_FIELDS',"query":"status:live","offset":0,"limit":10,"suppressdrafts":false}}]'
+curl -sG 'http://clamo.ftdata.co.uk/api'  -o 'test/stubs/cameron.json' --data-urlencode 'request=[{"action":"search","arguments":{"sort":"date","outputfields":'$OUTPUT_FIELDS',"query":"cameron","offset":0,"limit":10,"suppressdrafts":false}}]'
 curl -sG 'http://clamo.ftdata.co.uk/api'  -o 'test/stubs/nokia.json' --data-urlencode 'request=[{"action":"search","arguments":{"sort":"date","outputfields":'$OUTPUT_FIELDS',"query":"company:Nokia","offset":0,"limit":10,"suppressdrafts":false}}]'
 curl -sG 'http://clamo.ftdata.co.uk/api'  -o 'test/stubs/post.json' --data-urlencode 'request=[{"action":"getPost","arguments":{"id": 159222,"outputfields":'$OUTPUT_FIELDS'}}]'
 curl -sG 'http://clamo.ftdata.co.uk/api'  -o 'test/stubs/notfound.json' --data-urlencode 'request=[{"action":"getPost","arguments":{"id": "fail","outputfields":'$OUTPUT_FIELDS'}}]'
